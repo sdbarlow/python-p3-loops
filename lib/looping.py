8070697e4f0a68715fee164f9ce882324a6d67e3
sdbarlow <sethdbarlow@gmail.com>
2023-03-12 22:22:39 -0600

    all tests pass

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    for i in range(10, 0, -1):
+        print(i)
+        
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    return [i ** 2 for i in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
+    i = 1
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else: 
+            print(i)
     pass
